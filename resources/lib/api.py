b408a9b3d5a7bfbe5f707a6cbcfa35ccfcdc7f9b
daveesplana <daveesplana@outlook.com>
2026-06-25 13:21:58 +0800

    Changed IPTV_CHANNEL_IDS

--- resources/lib/api.py
+++ resources/lib/api.py
@@ -68,9 +68,9 @@ LIVE_CHANNELS = [
 ]
 
 IPTV_CHANNEL_IDS = {
-    'kctv': 'KoreanCentralTelevision.kp',
-    'kcbs': 'KoreanCentralBroadcastingStation.kp',
-    'vok':  'VoiceOfKorea.kp',
+    'kctv': 'KCTV',
+    'kcbs': 'KCBS',
+    'vok':  'VOK',
 }
 
 UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36'
